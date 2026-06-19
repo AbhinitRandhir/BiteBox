0a54dfa6c73aa992bf092089bf00475c96ced96f
AbhinitRandhir <abhinitk045@gmail.com>
2026-06-19 23:48:26 +0530

    login

---
+++ Bite-Box-site/js/scripts.js
@@ -0,0 +1,17 @@
+function login() 
+{
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let error = document.getElementById("error");
+
+    if(email === "admin@123" && password === "pass123") {
+
+        localStorage.setItem("isLoggedIn","true");
+        localStorage.setItem("user",email);
+
+        window.location.href="index.html";
+    } 
+    else {
+        error.innerText = "Invalid email and password";
+    }
+}
